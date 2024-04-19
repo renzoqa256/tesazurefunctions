772a544897f2ae5d57cac444b5c42ba9e80c1d65
renzoqa256 <renzoqa256@gmail.com>
2024-04-19 10:45:30 -0500

    Update HttpExample.js
    "bro" added

--- src/functions/HttpExample.js
+++ src/functions/HttpExample.js
@@ -8,6 +8,6 @@ app.http('HttpExample', {
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hello bro, ${name}!` };
     }
 });
